56779b63f4ed5b9b7ca21b70b1896a29fdb0dbf4
Houston Mannis <73046984+MannCave@users.noreply.github.com>
2025-08-21 22:31:54 -0500

    Create changing variables.js

---
+++ Java/changing variables.js
@@ -0,0 +1,26 @@
+let adjective;
+adjective = "hairy";
+let noun;
+noun = "Ozzy";
+let verb;
+verb = "smells";
+let place;
+place = "home";
+let adjective2;
+adjective2 = "stinky";
+let noun2;
+noun2 = "turkey";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "stinky";
+noun = "Ozzy";
+verb = "smells";
+place = "home";
+adjective2 = "horrible";
+noun2 = "turkey";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory)
