1c681280b8194fe5b2b45fbbaf7c051501955861
Houston Mannis <73046984+MannCave@users.noreply.github.com>
2025-08-21 22:21:43 -0500

    Create trivia.js

---
+++ Java/trivia.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Grok"; 
+let botLocation = "Mars"; 
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "The world's favorite language is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = botName + "'s favorite langauge is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "Bot's favorite langauge is " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
